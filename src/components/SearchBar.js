3b2bae078d1d9f528b1df01e54f7bf5bd9372dac
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-20 11:19:19 -0300

    feat: adds style to searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,33 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {StyleSheet, TextInput, View} from "react-native";
 import {Feather} from '@expo/vector-icons';
 
 const SearchBar = () => {
-	return (
-		<View style={styles.background}>
-			<Feather name="search" size={30} />
-			<Text>Search Bar</Text>
-		</View>
-	);
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle}/>
+            <TextInput style={styles.inputStyle} placeholder={"Search"}/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	background: {
-		backgroundColor: "#F0EEEF",
-		height: 50,
-		borderRadius: 5,
-		marginHorizontal: 15,
-	},
+    backgroundStyle: {
+        backgroundColor: "#F0EEEF",
+        height: 50,
+        borderRadius: 5,
+        margin: 15,
+        flexDirection: 'row',
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+    }
 });
 
 export default SearchBar;
