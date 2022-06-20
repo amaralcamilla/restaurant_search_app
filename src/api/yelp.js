3a6f11845c3f7ed5b5ff35e3b51ea9449127df25
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-20 17:11:42 -0300

    feat: makes a request to searchApi

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses'
+    baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization:
             'Bearer tEFLn5mhF81kJOwfJW3LQCD0pipVwLQiEzeGAMzEohm7A1oVkuDCDQ33cEwdXnr51ESBAQKR9C3TE2fHwZu3pVEESJ_IybFz2o24le_emdHObV1VbYlcj0zAfrqsYnYx'
