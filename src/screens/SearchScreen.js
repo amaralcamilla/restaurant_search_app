c066543cb13a143d213f2708f47924c9d491d9fd
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-20 12:05:57 -0300

    feat: adds state to searchBar

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import {StyleSheet, Text, View} from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
-	return (
-		<View>
-			<SearchBar />
-			<Text>SearchScreen</Text>
-		</View>
-	);
+    const [term, setTerm] = useState('');
+
+    return (
+        <View>
+            <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)}/>
+            <Text>SearchScreen</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
