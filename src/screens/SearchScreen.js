43cfb5e3cfd2f77e4db58446a977b6b3d6d70610
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-21 16:04:59 -0300

    chore: grouping results with filter by price

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,12 @@ const SearchScreen = () => {
     const [term, setTerm] = useState("");
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
     return (
         <View>
             <SearchBar
@@ -18,9 +24,9 @@ const SearchScreen = () => {
             {errorMessage ? <Text style={styles.errorStyle}>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results.</Text>
 
-            <ResultsList title={"Cost Effective"}/>
-            <ResultsList title={"Bit Pricier"}/>
-            <ResultsList title={"Big Spender"}/>
+            <ResultsList title={"Cost Effective"} results={filterResultsByPrice('$')}/>
+            <ResultsList title={"Bit Pricier"} results={filterResultsByPrice('$$')}/>
+            <ResultsList title={"Big Spender"} results={filterResultsByPrice('$$$')}/>
         </View>
     );
 };
