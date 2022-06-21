6aaf700af6e18c180d38bf163965fa338ab6c5a2
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-21 11:48:04 -0300

    feat: initializes ResultsList component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {StyleSheet, Text, View} from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("");
@@ -16,6 +17,10 @@ const SearchScreen = () => {
             />
             {errorMessage ? <Text style={styles.errorStyle}>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results.</Text>
+
+            <ResultsList title={"Cost Effective"}/>
+            <ResultsList title={"Bit Pricier"}/>
+            <ResultsList title={"Big Spender"}/>
         </View>
     );
 };
